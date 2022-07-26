5a9dcceb8b00a9cbb88f4f5dfdd23ab0116927a3
Chabrik Abdellatif <abdoo.chbrik@gmail.com>
2022-07-26 23:55:57 +0200

    service implementation

--- src/main/java/com/example1/demo/service/UserServiceImplementation.java
+++ src/main/java/com/example1/demo/service/UserServiceImplementation.java
@@ -2,32 +2,47 @@ package com.example1.demo.service;
 
 import com.example1.demo.domain.AppUser;
 import com.example1.demo.domain.Role;
+import com.example1.demo.repo.RoleRepo;
+import com.example1.demo.repo.UserRepo;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
 import java.util.List;
 
+@Service @RequiredArgsConstructor @Transactional @Slf4j
+
 public class UserServiceImplementation implements UserService{
+
+    private UserRepo appUserRepo;
+    private RoleRepo roleRepo;
+
     @Override
     public AppUser saveUser(AppUser user) {
-        return null;
+        log.info("saving new user to the database");
+        return appUserRepo.save(user);
     }
 
     @Override
     public Role saveRole(Role role) {
-        return null;
+        return roleRepo.save(role);
     }
 
     @Override
     public void AddRoleToUser(String username, String rolename) {
-
+       AppUser user = appUserRepo.findByUsername(username);
+       Role role = roleRepo.findByName(rolename);
+       user.getRoles().add(role);
     }
 
     @Override
     public AppUser getUser(String username) {
-        return null;
+        return appUserRepo.findByUsername(username);
     }
 
     @Override
     public List<AppUser> getUsers() {
-        return null;
+        return appUserRepo.findAll();
     }
 }
