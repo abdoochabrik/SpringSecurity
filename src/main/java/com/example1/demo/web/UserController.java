47010154db7170f1b0d32b7c2cef040ea62a3245
Chabrik Abdellatif <abdoo.chbrik@gmail.com>
2022-07-27 00:30:00 +0200

    web controllers

---
+++ src/main/java/com/example1/demo/web/UserController.java
@@ -0,0 +1,23 @@
+package com.example1.demo.web;
+
+import com.example1.demo.domain.AppUser;
+import com.example1.demo.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController @RequiredArgsConstructor @RequestMapping("/api")
+public class UserController {
+
+    private final UserService userService;
+
+    @GetMapping ("/users")
+    public ResponseEntity<List<AppUser>> getUsers() {
+        return ResponseEntity.ok().body(userService.getUsers());
+    }
+
+}
